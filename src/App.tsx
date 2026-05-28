470a6174bcd07ff1192e58e9d179957b4db1c575
Полина <tatarinova.polya2017@yandex.ru>
2026-05-28 20:43:01 +0300

    search

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,14 @@ function App() {
     return save ? JSON.parse(save) : [];
   };
 
+  const localStorageSort = () => {
+    const save = localStorage.getItem("user");
+    return save ? JSON.parse(save) : "";
+  };
+
   const [user, setUser] = useState(localStorageUser);
   const [value, setValue] = useState(localStorageValue);
+  const [sort, setSort] = useState(localStorageSort);
   const USERS = "https://jsonplaceholder.typicode.com/users";
 
   useEffect(() => {
@@ -44,10 +50,25 @@ function App() {
     );
   });
 
+  const sortUserNameOrCity = (arraySort) => {
+    if (sort === "name") {
+      return [...arraySort].sort((a, b) => a.name.localeCompare(b.name));
+    }
+    if (sort === "city") {
+      return [...arraySort].sort((a, b) =>
+        a.address.city.localeCompare(b.address.city),
+      );
+    }
+    return arraySort
+  };
+
+  const displayUserSortFilter = sortUserNameOrCity(inputFilterUsers);
+
   useEffect(() => {
     localStorage.setItem("value", JSON.stringify(value));
     localStorage.setItem("user", JSON.stringify(user));
-  }, [value, user]);
+    localStorage.setItem("sort", JSON.stringify(sort));
+  }, [value, user, sort]);
 
   return (
     <>
@@ -60,9 +81,11 @@ function App() {
         />
         {/* <button className="search-button">найти</button> */}
       </div>
+      <button onClick={()=>{setSort('name')}}>Сортировать по имени(в алфавитном порядке)</button>
+      <button onClick={()=>{setSort('city')}}>Сортировать по городу(в алфавитном порядке)</button>
 
       <div className="cards-grid">
-        {inputFilterUsers.map((person) => (
+        {displayUserSortFilter.map((person) => (
           <div className="card" key={person.id}>
             <p className="card-title">{person.name}</p>
             <p className="card-email">{person.email}</p>
