06ee9df2b30d9e08d70e0dd588013e8c83fa2c40
Полина <tatarinova.polya2017@yandex.ru>
2026-05-28 19:35:21 +0300

    search

---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const localStorageValue = () => {
+    const save = localStorage.getItem("value");
+    return save ? JSON.parse(save) : "";
+  };
+
+  const localStorageUser = () => {
+    const save = localStorage.getItem("user");
+    return save ? JSON.parse(save) : [];
+  };
+
+  const [user, setUser] = useState(localStorageUser);
+  const [value, setValue] = useState(localStorageValue);
+  const USERS = "https://jsonplaceholder.typicode.com/users";
+
+  useEffect(() => {
+    async function fetchServerData() {
+      try {
+        const responce = await fetch(USERS);
+        if (!responce.ok) {
+          throw new Error(`Ошибка сервера: ${responce.status}`);
+        }
+        const data = await responce.json();
+        setUser(data);
+        console.log("Полученные данные:", data);
+      } catch (error) {
+        console.error("Ошибка при загрузке данных:", error);
+      }
+    }
+    fetchServerData();
+  }, []);
+
+  const inputFilterUsers = user.filter((person) => {
+    if (!value.trim()) {
+      return true;
+    }
+    const inputValue = value.toLowerCase();
+    return (
+      person.name.toLowerCase().includes(inputValue) ||
+      person.address.city.toLowerCase().includes(inputValue)
+    );
+  });
+
+  useEffect(() => {
+    localStorage.setItem("value", JSON.stringify(value));
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [value, user]);
+
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+          className="search-input"
+        />
+        {/* <button className="search-button">найти</button> */}
+      </div>
+
+      <div className="cards-grid">
+        {inputFilterUsers.map((person) => (
+          <div className="card" key={person.id}>
+            <p className="card-title">{person.name}</p>
+            <p className="card-email">{person.email}</p>
+            <p className="card-city">{person.address.city}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
